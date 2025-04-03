0e63fa57b8ee724c190594dc31706bd3625f3943
joao victor <bryanrubim123@gmail.com>
2025-04-02 22:09:52 -0300

    mundanças

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
-nome = String
-sobrenome = String
-idade = Number
-maiorDeIdade = Boolean
-altura = float 
 
-const nome = joao
-const sobrenome = rubim
-const = idade 
-const maior de idade == 18 
+const nome = "joao";
+const sobrenome = "rubim";
+const idade = 19; 
+const maiorDeIdade =  true; 
 const altura = 1.78
+
+console.log("ola meu nome", nome,  "mas me chamam de" , sobrenome, " tenho ",+idade, "e de altura", +altura, );
